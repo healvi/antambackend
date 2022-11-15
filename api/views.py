f18bd980ff177586ecfd064ae5ec8305b867657e
Andriansyah Putra <muhammad.andriansyah@ui.ac.id>
2022-11-15 13:32:23 +0700

    testing for hosting

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from email import message
+from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics, status
@@ -9,6 +11,20 @@ from .serializers import UserSerializers, WablasSerializers
 # Create your views here.
 import pandas
 import json
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from urllib.parse import quote
+import os
+
+options = Options()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument("--profile-directory=Default")
 
 
 @api_view(['GET', 'POST'])
@@ -26,9 +42,48 @@ def students_list(request):
         fss = FileSystemStorage()
         file = fss.save(upload.name, upload)
         file_url = fss.url(file)
-        excel_data_df = pandas.read_excel(upload)
-        thisisjson = excel_data_df.to_json(orient='records')
-        return Response({'file_url': thisisjson})
+        df = pandas.read_excel(("."+file_url), header=0)
+        
+        # os.system("")
+        # os.environ["WDM_LOG_LEVEL"] = "0"
+        
+        options = Options()
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        options.add_argument("--profile-directory=Default")
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        delay = 30
+        driver.get('https://web.whatsapp.com')
+        
+        name_phone_dict = dict(zip(df['nama'].tolist(), df['no_telp'].tolist()))
+        for _, (name, phone) in enumerate(name_phone_dict.items()):
+            try:
+                message = "Hi" + " " + name
+                phone = "+" + str(phone)
+                url = 'https://web.whatsapp.com/send?phone=' + str(phone) + '&text=' + message
+                sent = False
+                for i in range(3):
+                    if not sent:
+                        driver.get(url)
+                        try:
+                            click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='compose-btn-send']")))
+                        except Exception as e:
+                            print(f"\nFailed to send message to: {str(phone)}, retry ({i+1}/3)")
+                            print("Make sure your phone and computer is connected to the internet.")
+                            print("If there is an alert, please dismiss it.")
+                        else:
+                            sleep(1)
+                            click_btn.click()
+                            sent=True
+                            sleep(3)
+            except Exception as e:
+                print('Failed to send message to ' + str(phone) + str(e))
+            # print(k, v)
+        driver.close()
+                
+        res = df.to_json(orient="columns")
+        # parsed = json.loads(res)
+        parsed = json.dumps(res)
+        return Response({'response': json.dumps(parsed)}, content_type="application/json")
 
 
 # @api_view(['PUT', 'DELETE'])
